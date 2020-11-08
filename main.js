248312e7966c4383a4cf0ebc7cae1e69eaeec2e8
Abdoahmed111 <eng.abdelrahman.ahmed11@gmail.com>
2020-11-08 13:59:37 +0200

    change the layout and some styles

--- main.js
+++ main.js
@@ -1,15 +1,84 @@
-var swiper = new Swiper(".swiper-container", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  slidesPerGroup: 1,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+// owl carousel
+$(document).ready(function () {
+  // MAIN SLIDER
+  $(".main-slider").owlCarousel({
+    loop: true,
+    margin: 0,
+    responsiveClass: false,
+    responsive: false,
+    items: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    smartSpeed: 500,
+    autoplayHoverPause: true,
+  });
+  // PRODUCTS SLIDER
+  $(".product-slider").owlCarousel({
+    loop: true,
+    margin: 25,
+    responsiveClass: true,
+
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 4,
+        nav: true,
+      },
+    },
+    autoplay: true,
+    autoplaySpeed: 1000,
+    smartSpeed: 500,
+    autoplayHoverPause: true,
+  });
+  // TESTIMONIALS-SLIDER
+  $(".testimonials-slide").owlCarousel({
+    loop: true,
+    margin: 20,
+    responsiveClass: true,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: true,
+      },
+      600: {
+        items: 2,
+        nav: false,
+        dots: true,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+  // COMPANIES SLIDER
+  $(".companies-slider").owlCarousel({
+    loop: true,
+    margin: 40,
+    responsiveClass: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      850: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
 });
